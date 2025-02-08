4148440144ef3568122a095ce193cb9c7034651b
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-08 14:43:55 +0300

    Файл конфигурации установки виджета

---
+++ config/.install.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Файл конфигурации установки виджета.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'use'         => FRONTEND,
+    'id'          => 'gm.wd.tagcloud',
+    'category'    => 'tag',
+    'name'        => 'Tag cloud',
+    'description' => 'Display tags as a cloud',
+    'namespace'   => 'Gm\Widget\TagCloud',
+    'path'        => '/gm/gm.wd.tagcloud',
+    'locales'     => ['ru_RU', 'en_GB'],
+    'events'      => [],
+    'required'    => [
+        ['php', 'version' => '8.2'],
+        ['app', 'code' => 'GM CMS']
+    ]
+];
