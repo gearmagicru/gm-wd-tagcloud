0ce16e8daf969e7c80d9cba5654a0a072e61f391
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-08 14:46:52 +0300

    Файлы локализации

---
+++ lang/text-ru_RU.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Облако меток',
+    '{description}' => 'Отображение меток в виде облака',
+];
